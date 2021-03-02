c1aafb11b4bdc180030d6d93b6e3063daec3096d
arifusr <arifusr@gmail.com>
2021-03-02 08:56:48 +0700

    getting started

--- electron_doc.mm
+++ electron_doc.mm
@@ -2,7 +2,7 @@
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <node TEXT="electron_doc" FOLDED="false" ID="ID_1424151504" CREATED="1614649937563" MODIFIED="1614649953793" STYLE="oval">
 <font SIZE="18"/>
-<hook NAME="MapStyle">
+<hook NAME="MapStyle" zoom="1.331">
     <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
 
 <map_styles>
@@ -63,6 +63,30 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Quickstart" POSITION="right" ID="ID_1635499081" CREATED="1614650048920" MODIFIED="1614650049733">
+<edge COLOR="#ff0000"/>
+<node TEXT="Quick Start Guide" ID="ID_718180013" CREATED="1614650073599" MODIFIED="1614650074775">
+<node TEXT="Prerequisites" ID="ID_683605966" CREATED="1614650158448" MODIFIED="1614650158914"/>
+<node TEXT="Create a basic application" ID="ID_884312503" CREATED="1614650165675" MODIFIED="1614650165954"/>
+<node TEXT="Run your application" ID="ID_860031777" CREATED="1614650169625" MODIFIED="1614650169859"/>
+<node TEXT="Package and distribute the application" ID="ID_818685217" CREATED="1614650173434" MODIFIED="1614650183171"/>
+</node>
+</node>
+<node TEXT="Learning the basics" POSITION="right" ID="ID_1584788488" CREATED="1614650057114" MODIFIED="1614650057501">
+<edge COLOR="#0000ff"/>
+<node TEXT="Electron&apos;s Process Model" ID="ID_159623011" CREATED="1614650083495" MODIFIED="1614650084633"/>
+<node TEXT="Adding Features to Your App" ID="ID_1668583527" CREATED="1614650089724" MODIFIED="1614650090006"/>
+<node TEXT="Boilerplates and CLIs" ID="ID_676107038" CREATED="1614650102894" MODIFIED="1614650103194"/>
+</node>
+<node TEXT="Advanced steps" POSITION="right" ID="ID_1419416951" CREATED="1614650063382" MODIFIED="1614650063678">
+<edge COLOR="#00ff00"/>
+<node TEXT="Application Architecture" ID="ID_4218170" CREATED="1614650124113" MODIFIED="1614650124444"/>
+<node TEXT="Accessibility" ID="ID_277053333" CREATED="1614650130911" MODIFIED="1614650131231"/>
+<node TEXT="Testing and Debugging" ID="ID_311885472" CREATED="1614650136271" MODIFIED="1614650136534"/>
+<node TEXT="Distribution" ID="ID_1840444485" CREATED="1614650141518" MODIFIED="1614650141782"/>
+<node TEXT="Updates" ID="ID_1116408253" CREATED="1614650145857" MODIFIED="1614650146159"/>
+<node TEXT="Getting Support" ID="ID_1205917594" CREATED="1614650149714" MODIFIED="1614650150050"/>
+</node>
 </node>
 </map>
